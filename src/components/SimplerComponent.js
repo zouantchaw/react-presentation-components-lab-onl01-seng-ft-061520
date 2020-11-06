bc056edb5459919f518952950cc46099d911b7fc
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-11-06 14:02:05 -0500

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,15 @@
-// Code SimplerComponent Here
+import React from 'react';
+
+// const SimplerComponent = (props) => {
+//     return (
+//         <div onClick={props.handleClick}>
+//             I am just happy
+//         </div>
+//     );
+// };
+
+const SimplerComponent = props => <div onClick={props.handleClick}>I am just happy</div>
+
+export default SimplerComponent;
+
+
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => {console.log("TESTTTTTTT")}} />
   </div>,
   document.getElementById('root')
 );
